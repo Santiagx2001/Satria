5e9587d21c0d4b92b064eec0623a961ee260036b
Santiago <ssalazarb918@gmail.com>
2025-05-11 22:16:23 -0500

    Practica de React

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { PrimerComponente } from './components/PrimerComponente'
+import { SegundoComponente } from './components/SegundoComponente'
+
+function App() {
+
+  return (
+    <>
+      <PrimerComponente />
+      <SegundoComponente  />
+    </>
+  )
+}
+
+export default App
