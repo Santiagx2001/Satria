dc5b79f6aadea5503b8884f5f76037cd9d2371cf
Santiagx2001 <ssalazarb918@gmail.com>
2025-07-04 10:21:33 -0500

    /

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import {Route, Routes} from 'react-router-dom'
-import { Banner } from './components/sections/Banner'
-import { QuienesSomos } from './components/sections/QuienesSomos'
-import { Header } from './components/sections/Header'
-import { Productos } from './components/sections/Productos'
-
+import { Banner } from "./components/sections/Banner";
+import { QuienesSomos } from "./components/sections/QuienesSomos";
+import { Header } from "./components/sections/Header";
+import { Productos } from "./components/sections/Productos";
 
 function App() {
-
   return (
-    <div className='bg-AmarilloClaro-Satria'>
+    <div className="bg-AmarilloClaro-Satria">
       <Header />
       <Banner />
       <QuienesSomos />
       <Productos />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/routes/router.js
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+const Rutas = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<div>Página de inicio</div>} />
+        </Routes>
+    );
+}
+
+export default Rutas;
